8ad0930f7724a004c398fca55fe99835fc18cd07
Zouhair-Al-Yazji <alyazjyzhyr@gmail.com>
2025-09-13 15:56:14 +0300

    fix the user type

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -3,11 +3,11 @@ import NextAuth, { DefaultSession } from "next-auth";
 declare module "next-auth" {
   interface Session {
     user: {
-      guestId?: string;
+      guestId?: number;
     } & DefaultSession["user"];
   }
 
   interface User {
-    guestId?: string;
+    guestId?: number;
   }
 }
