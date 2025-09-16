acb0c7e90deb9403c570e696d32bd01edfbd3415
Zouhair-Al-Yazji <alyazjyzhyr@gmail.com>
2025-09-16 15:09:39 +0300

    add a title for SignOut button

--- components/SignOutButton.tsx
+++ components/SignOutButton.tsx
@@ -7,6 +7,7 @@ export default function SignOutButton() {
   return (
     <button
       onClick={() => signOut({ redirectTo: "/" })}
+      title="Sign out"
       className="hover:bg-primary-900 hover:text-primary-100 text-primary-200 flex w-full cursor-pointer items-center gap-4 px-3 py-1.5 font-semibold transition-colors sm:px-5 sm:py-3"
     >
       <ArrowRightStartOnRectangleIcon className="text-primary-600 h-5 w-5" />
